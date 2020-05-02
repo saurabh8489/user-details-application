5ab6811a8bffe355a896e940c930c0f00acdf8e3
Saurabh <saurabh8489@gmail.com>
2020-05-02 17:59:43 +0530

    Added integration of userDetails module to api gateway

--- src/main/java/com/user/userdetails/controller/UserDetailsController.java
+++ src/main/java/com/user/userdetails/controller/UserDetailsController.java
@@ -2,8 +2,8 @@ package com.user.userdetails.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +11,13 @@ import com.user.userdetails.service.UserDetailsService;
 import com.user.userdetails.service.UserResponse;
 
 @RestController
-@RequestMapping(path = "/userdetails")
+@RequestMapping(path = "/user")
 public class UserDetailsController {
 	public static String MODULE_NAME = "User-Details-Application";
 	@Autowired
 	public UserDetailsService detailsService;
 
-	@GetMapping
+	@RequestMapping(value = "/details", method = RequestMethod.GET)
 	public ResponseEntity<UserResponse> getUserDetails(@RequestParam(name = "userName") String userName,
 			@RequestParam(name = "userDob") String userDob) {
 		String userInfo = detailsService.getUser(userName, userDob);
